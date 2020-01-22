25a802f141afe2a9170840d0fe7e40e2bf07389e
Kirill Makarin <kirmakarin@gmail.com>
2020-01-23 00:06:53 +0300

    Clear imports block in Main class

--- src/main/java/su/netbox/Main.java
+++ src/main/java/su/netbox/Main.java
@@ -5,7 +5,6 @@ import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicCredentialsProvider;
