89d8a2c8a32b5a393c963b0819c7638d808aab32
Kirill Makarin <kirmakarin@gmail.com>
2020-01-24 00:41:36 +0300

    Refactor Main class: 1) using ProxyDTO 2) fix result format

--- src/main/java/su/netbox/Main.java
+++ src/main/java/su/netbox/Main.java
@@ -13,33 +13,76 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        List<ProxyDTO> proxies = new ArrayList<ProxyDTO>();
+        List<ProxyDTO> brokenProxies = new ArrayList<ProxyDTO>();
+
+        proxies.add(new ProxyDTO("194.183.168.4", "20028", "user7930", "koo5Vahx"));
+
+        //toDo: add logic for fetch proxies from txt file
+        for (ProxyDTO proxy : proxies) {
+            //be informed that if checkProxy return false
+            // it is not necessary that the proxy is not working (disconnection, url target is not correct, etc.)
+            if (!checkProxy(proxy)) {
+                brokenProxies.add(proxy);
+            }
+        }
+        if (!brokenProxies.isEmpty()) {
+            int workingProxyCounter = proxies.size() - brokenProxies.size();
+            printResult(workingProxyCounter, proxies.size(), brokenProxies);
+        } else {
+            System.out.println("100% of proxies are working");
+        }
+
+
+    }
+
+    private static Boolean checkProxy(ProxyDTO proxy) {
         CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
 
-        credentialsProvider.setCredentials(new AuthScope("194.183.168.4", 20028), new
-                UsernamePasswordCredentials("user7930", "koo5Vahx"));
+        credentialsProvider.setCredentials(new AuthScope(proxy.getHost(), Integer.parseInt(proxy.getPort())),
+                new UsernamePasswordCredentials(proxy.getLogin(), proxy.getPassword()));
 
         HttpClientBuilder clientBuilder = HttpClients.custom();
         clientBuilder = clientBuilder.setDefaultCredentialsProvider(credentialsProvider);
 
         CloseableHttpClient httpClient = clientBuilder.build();
-        HttpHost proxyHost = new HttpHost("194.183.168.4", 20028, "http");
+        //toDo: Add opportunity to select default protocol
+        HttpHost proxyHost = new HttpHost(proxy.getHost(), Integer.parseInt(proxy.getPort()),
+                proxy.getProtocol() != null ? proxy.getProtocol() : "http");
 
         RequestConfig.Builder requestConfigBuilder = RequestConfig.custom();
         requestConfigBuilder = requestConfigBuilder.setProxy(proxyHost);
         RequestConfig config = requestConfigBuilder.build();
 
+        //toDo: Add opportunity to select target
         HttpGet httpGet = new HttpGet("https://www.instagram.com/1rich_dad1/");
         httpGet.setConfig(config);
 
         try {
             HttpResponse httpResponse = httpClient.execute(httpGet);
-            System.out.println(httpResponse.getStatusLine());
-            httpClient.close();
+            //toDO:  add logic for another response code
+            if (httpResponse.getStatusLine().getStatusCode() == 200) { //toDo: take away const to interface
+                httpClient.close();
+                return true;
+            } else {
+                httpClient.close();
+                return false;
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return false;
     }
+
+    private static void printResult(int workingProxyCounter, int numberOfProxy, List<ProxyDTO> brokenProxies) {
+        System.out.println(workingProxyCounter / numberOfProxy * 100 + "% of working proxy");
+        brokenProxies.forEach(proxy -> System.out.println(proxy.toString()));
+    }
+
 }
